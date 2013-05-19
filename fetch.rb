af3dda7a4cbed192bce85b8b8464c691a92578ef
Bennett Wilson <bennettwilson@gmail.com>
2013-05-19 15:01:11 -0400

    changing puts to hash

--- fetch.rb
+++ fetch.rb
@@ -12,14 +12,22 @@ playlists = @session.container.contents
 playlist = playlists.first
 #playlists.each do |playlist|
 
-  
-  #playlist.load unless playlist.loaded?
-  puts "#{playlist.name} - #{playlist.class.name}"
+  playlist_json = Hash.new
+  #puts "#{playlist.name} - #{playlist.class.name}"
+  playlist_json[:playlist_name] = playlist.name
+  playlist_json[:song_count] = playlist.size
+  playlist_json[:spotify_uri] = playlist.to_link
+
   tracks = playlist.tracks
+  tracks_json = {}
   tracks.each do |track|
-    puts "  #{track.artist.name} - #{track.album.name} (#{track.album.release_year}) - #{track.name}"
+    #puts "  #{track.artist.name} - #{track.album.name} (#{track.album.release_year}) - #{track.name}"
+    tracks_json[track.to_link] = [:title => track.name, :artist => track.artist.name, :album => track.album.name]
   end
+
+  playlist_json[:tracks] = tracks_json
+  puts JSON.pretty_generate playlist_json
 
 #end
 
-@session.logout!
+#@session.logout!
