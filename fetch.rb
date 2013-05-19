a01ec335ae6bcefd6d38545b7f6f3700525d6d71
Bennett Wilson <bennettwilson@gmail.com>
2013-05-19 15:48:15 -0400

    adding output functionality to out/ dir

--- fetch.rb
+++ fetch.rb
@@ -10,24 +10,28 @@ puts "Logged in as #{@session.user.display_name}"
 
 playlists = @session.container.contents
 playlist = playlists.first
-#playlists.each do |playlist|
+playlists.each do |playlist|
 
+  next if playlist.instance_of? Hallon::PlaylistContainer::Folder
   playlist_json = Hash.new
   #puts "#{playlist.name} - #{playlist.class.name}"
   playlist_json[:playlist_name] = playlist.name
   playlist_json[:song_count] = playlist.size
-  playlist_json[:spotify_uri] = playlist.to_link
+  playlist_json[:spotify_uri] = playlist.to_link.to_s
 
   tracks = playlist.tracks
   tracks_json = {}
   tracks.each do |track|
     #puts "  #{track.artist.name} - #{track.album.name} (#{track.album.release_year}) - #{track.name}"
-    tracks_json[track.to_link] = [:title => track.name, :artist => track.artist.name, :album => track.album.name]
+    tracks_json[track.to_link] = [:title => track.name, :artist => track.artist.name, :album => track.album.name, :date_added => track.added_at, :index => track.index]
   end
 
   playlist_json[:tracks] = tracks_json
-  puts JSON.pretty_generate playlist_json
+  #puts JSON.pretty_generate playlist_json
+  File.open("out/#{playlist.name.gsub('/', '-')}.json", "w") do |file|
+    file.write JSON.pretty_generate playlist_json
+  end
+  puts "#{playlist.name}"
 
-#end
+end
 
-#@session.logout!
